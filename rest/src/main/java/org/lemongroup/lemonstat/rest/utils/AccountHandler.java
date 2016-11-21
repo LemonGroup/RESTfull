7e7ec36ea1f7b58828bcff71a9e708048d23be42
tatpetrova <vipstud1@yandex.ru>
2016-11-21 19:55:07 +0300

    Methods refactoring

--- rest/src/main/java/org/lemongroup/lemonstat/rest/utils/AccountHandler.java
+++ rest/src/main/java/org/lemongroup/lemonstat/rest/utils/AccountHandler.java
@@ -16,34 +16,37 @@ public class AccountHandler {
     private AccountHandler() {
     }
 
-    public static AccountHandler getInstance(){
-	if(instance == null){
-	    instance = new AccountHandler();
-	    accountRepo = FakeAccountRepository.getInstance();
-	}
-	return instance;
+    public static AccountHandler getInstance() {
+        if (instance == null) {
+            instance = new AccountHandler();
+            accountRepo = FakeAccountRepository.getInstance();
+        }
+        return instance;
     }
-    public static boolean auth(Map<String,String> authParams) {
-	long uId = accountRepo.getUserIdByUserName(authParams.get("user"));
-	System.out.println("uid: " + uId);
-	if(uId == 0) {
-	    //No such user
-	    return false;
-	} else {
-	    //Compare passwords
-	    if(authParams.get("pass").equals(accountRepo.getPasswordByUserId(uId))){
-		return true;
-	    }
-	}
-	return false;
+
+    public boolean auth(Map<String, String> authParams) {
+        long uId = accountRepo.getUserIdByUserName(authParams.get("user"));
+        System.out.println("uid: " + uId);
+        if (uId == 0) {
+            //No such user
+            return false;
+        } else {
+            //Compare passwords
+            if (authParams.get("pass").equals(accountRepo.getPasswordByUserId(uId))) {
+                return true;
+            }
+        }
+        return false;
     }
-    public static AuthResponse getAuthResponse(String user) {
-	String token = accountRepo.getTokenByUser(user);
-	long groupId = accountRepo.getGroupIdByUser(user);
-	byte privilege = accountRepo.getPrivilegeByUser(user);
-	return new AuthResponse(token,groupId,privilege);
+
+    public AuthResponse getAuthResponse(String user) {
+        String token = accountRepo.getTokenByUser(user);
+        long groupId = accountRepo.getGroupIdByUser(user);
+        byte privilege = accountRepo.getPrivilegeByUser(user);
+        return new AuthResponse(token, groupId, privilege);
     }
-    public static long getGroupIdByToken(String token) { 
-	return accountRepo.getGroupIdByToken(token);
+
+    public long getGroupIdByToken(String token) {
+        return accountRepo.getGroupIdByToken(token);
     }
 }
