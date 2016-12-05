26da5ac306ef7ee2072cadb7ca8c098371f89565
lemongroup <lemongro1@uc2.nodecluster.net>
2016-12-05 07:42:33 -0600

    rutin commit

--- rest/src/main/java/org/lemongroup/lemonstat/rest/Application.java
+++ rest/src/main/java/org/lemongroup/lemonstat/rest/Application.java
@@ -3,9 +3,10 @@ package org.lemongroup.lemonstat.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application extends SpringBootServletInitializer implements CommandLineRunner {
 
     public static void main(String[] args) {
 	SpringApplication.run(Application.class, args);
